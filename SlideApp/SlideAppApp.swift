687cc6e507be2fba7e43a0be51bb708cfc133531
RyoKitatani <ryo0929m@gmail.com>
2021-05-07 10:53:32 +0900

    Initial Commit

---
+++ SlideApp/SlideAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideAppApp.swift
+//  SlideApp
+//
+//  Created by RyoKitatani on 2021/05/07.
+//
+
+import SwiftUI
+
+@main
+struct SlideAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
